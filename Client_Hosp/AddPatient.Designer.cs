e94cf3c7eb333a0d7fe39786079cc4469d76612f
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-11-23 23:14:13 +0100

    fix: add missing event handler for panel3.Paint in AddPatient form

--- Client_Hosp/AddPatient.Designer.cs
+++ Client_Hosp/AddPatient.Designer.cs
@@ -412,6 +412,7 @@
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(839, 215);
             this.panel3.TabIndex = 6;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
             // dateTimePicker1
             // 
