bde28996620fdd54c4e97366ca5f4016385ba0c1
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-11-24 06:54:24 +0100

    feat(Server_Hosp): add Specialization, DepartmentId, and Status properties to Patient class

--- Server_Hosp/Patient.cs
+++ Server_Hosp/Patient.cs
@@ -20,6 +20,9 @@ namespace Server_Hosp
         public int DoctorId { get; set; }
         public int RoomId { get; set; }
         public string Diagnosis { get; set; }
+        public string Specialization { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int DepartmentId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Status { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         #endregion
 
         #region Initialization Methods
@@ -182,102 +185,56 @@ namespace Server_Hosp
             command.Parameters.AddWithValue("@roomId", RoomId);
             command.Parameters.AddWithValue("@diagnosis", Diagnosis ?? (object)DBNull.Value);
         }
-        #endregion
 
-        #region Not Implemented Interface Members
-        // RPC Interface members that are not implemented for Patient class
-        public string Specialization { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int DepartmentId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Status { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        int RPC.ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        string RPC.FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        string RPC.LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        string RPC.PhoneNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        string RPC.Specialization { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        int RPC.DepartmentId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        string RPC.Address { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        string RPC.Gender { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        string RPC.Status { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public bool Login(string username, string password) => throw new NotImplementedException();
-        public string RegisterUser(string username, string password) => throw new NotImplementedException();
-        public List<RPC> GetDoctors(string connectionString) => throw new NotImplementedException();
-        public void Initialize(string text1, string text2, string text3, int v, string text4, string text5, string text6, string text7) => throw new NotImplementedException();
-        public void Initialize(int id, string firstName, string lastName, string phoneNumber, string specialization, int departmentId, string address, string gender, string status) => throw new NotImplementedException();
-        public string DeleteDoctor(string connectionString, int doctorId) => throw new NotImplementedException();
-        public string ModifyDoctor(string connectionString, int doctorId, string firstName, string lastName, string phoneNumber, string specialization, int departmentId, string address, string gender, string status) => throw new NotImplementedException();
-        public List<string> GetDepartments(string connectionString) => throw new NotImplementedException();
-        public List<string> GetSpecializations(string connectionString) => throw new NotImplementedException();
-
-        void RPC.Initialize(int id, string firstName, string lastName, string phoneNumber, string specialization, int departmentId, string address, string gender, string status)
+        public void Initialize(int id, string firstName, string lastName, string phoneNumber, string specialization, int departmentId, string address, string gender, string status)
         {
             throw new NotImplementedException();
         }
 
-        string RPC.Add(string connectionString)
+        public string DeleteDoctor(string connectionString, int doctorId)
         {
             throw new NotImplementedException();
         }
 
-        string RPC.DeleteDoctor(string connectionString, int doctorId)
+        public string ModifyDoctor(string connectionString, int doctorId, string firstName, string lastName, string phoneNumber, string specialization, int departmentId, string address, string gender, string status)
         {
             throw new NotImplementedException();
         }
 
-        List<RPC> RPC.GetAll(string connectionString)
+        public bool Login(string username, string password)
         {
             throw new NotImplementedException();
         }
 
-        string RPC.ModifyDoctor(string connectionString, int doctorId, string firstName, string lastName, string phoneNumber, string specialization, int departmentId, string address, string gender, string status)
+        public string RegisterUser(string username, string password)
         {
             throw new NotImplementedException();
         }
 
-        bool RPC.Login(string username, string password)
+        public void Initialize(string text1, string text2, string text3, int v, string text4, string text5, string text6, string text7)
         {
             throw new NotImplementedException();
         }
 
-        string RPC.RegisterUser(string username, string password)
+        public List<string> GetDepartments(string connectionString)
         {
             throw new NotImplementedException();
         }
 
-        void RPC.Initialize(string text1, string text2, string text3, int v, string text4, string text5, string text6, string text7)
+        public List<string> GetSpecializations(string connectionString)
         {
             throw new NotImplementedException();
         }
 
-        List<string> RPC.GetDepartments(string connectionString)
+        public List<RPC> GetDoctors(string connectionString)
         {
             throw new NotImplementedException();
         }
 
-        List<string> RPC.GetSpecializations(string connectionString)
-        {
-            throw new NotImplementedException();
-        }
-
-        List<RPC> RPC.GetDoctors(string connectionString)
-        {
-            throw new NotImplementedException();
-        }
-
-        void RPC.Initialize(int patientId, string v1, string v2, string v3, string v4, DateTime value, string v5, string v6, int v7, int v8, string v9)
-        {
-            throw new NotImplementedException();
-        }
-
-        string RPC.DeletePatient(string connectionString, int selectedPatientID)
-        {
-            throw new NotImplementedException();
-        }
-
-        string RPC.Update(string connectionString, int patientId, string v1, string v2, string v3, string v4, DateTime value, string v5, string v6, int v7, int v8, string v9)
+        public string Update(string connectionString, int patientId, string v1, string v2, string v3, string v4, DateTime value, string v5, string v6, int v7, int v8, string v9)
         {
             throw new NotImplementedException();
         }
         #endregion
-    }
-}
+
+    } }
