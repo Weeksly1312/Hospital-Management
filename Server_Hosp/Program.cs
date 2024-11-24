6a48081c8f9b883d3b1ef5a14088f70e3321a19c
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-11-24 06:18:42 +0100

    refactor: reorganize service registration logic into ServerManager class

--- Server_Hosp/Program.cs
+++ Server_Hosp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Runtime.Remoting.Channels.Tcp;
-using System.Runtime.Remoting.Channels;
-using System.Runtime.Remoting;
+using Server_Hosp.Utils;
 
 namespace Server_Hosp
 {
@@ -11,34 +9,7 @@ namespace Server_Hosp
         {
             try
             {
-                // Unregister any existing channels first
-                //foreach (var channel in ChannelServices.RegisteredChannels)
-                //{
-                //    ChannelServices.UnregisterChannel(channel);
-                //}
-
-                // Create and register new TCP channel
-                TcpChannel channel = new TcpChannel(2222);
-                ChannelServices.RegisterChannel(channel, false);
-
-                // Register service types
-                RemotingConfiguration.RegisterWellKnownServiceType(
-                    typeof(LoginService),
-                    "login",
-                    WellKnownObjectMode.Singleton);
-
-                RemotingConfiguration.RegisterWellKnownServiceType(
-                    typeof(Doctor),
-                    "doctor",
-                    WellKnownObjectMode.Singleton);
-
-                RemotingConfiguration.RegisterWellKnownServiceType(
-                   typeof(Server_Hosp.Patient),
-                   "patient",
-                   WellKnownObjectMode.Singleton);
-
-                Console.WriteLine("Server is running on port 2222!");
-                Console.WriteLine("Press Enter to exit...");
+                ServerManager.RegisterServices();
                 Console.ReadLine();
             }
             catch (Exception ex)
