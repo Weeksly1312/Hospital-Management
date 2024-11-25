5bc637b697cfd3e525218b1788b3939ea115aa95
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-11-25 13:19:03 +0100

    refactor(Program.cs): remove unnecessary comments from the Program class

--- Client_Hosp/Program.cs
+++ Client_Hosp/Program.cs
@@ -8,9 +8,6 @@ namespace Client_Hosp
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
