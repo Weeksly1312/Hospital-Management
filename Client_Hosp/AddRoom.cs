953f43cc99e48ed465d2665bb67606d5ea1cfe9f
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-11-25 21:54:22 +0100

    feat(Client_Hosp): add functionality to export room details in AddRoom form

--- Client_Hosp/AddRoom.cs
+++ Client_Hosp/AddRoom.cs
@@ -16,5 +16,15 @@ namespace Client_Hosp
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
